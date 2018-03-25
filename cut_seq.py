15775aa26472a684f07afc4b38051efb4f8ce92f
MMaetha <jiramaetha.s@ku.th>
2018-03-25 19:32:34 +0700

    minor change in cut_seq.py

--- cut_seq.py
+++ cut_seq.py
@@ -1,6 +1,5 @@
 from Bio import SeqIO
-for seq in SeqIO.parse("./sample_seq/IRGSP-1.0_genome.fasta", "fasta"):
-	print(seq.id)
-	print(len(seq))
-	print(repr(seq))
+from Bio.Seq import Seq
+for import_seq in SeqIO.parse("./sample_seq/IRGSP-1.0_genome.fasta", "fasta"):
+	print(repr(import_seq))
 #dt2 = data[76527-50:76527+51]
