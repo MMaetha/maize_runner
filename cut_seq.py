e5aa137a09e58e921bf3341e2e0e0a4d831bedd6
MMaetha <jiramaetha.s@ku.th>
2018-03-25 18:57:25 +0700

    read fasta file by biopython

--- cut_seq.py
+++ cut_seq.py
@@ -1,23 +1,6 @@
 from Bio import SeqIO
-
-count=0
-f = open('./sample_seq/IRGSP-1.0_genome.fasta','r')
-data = f.readlines()
-if(data=='>'):
-    count = count +1
-while(count<2):
-    break;
-f.close()
-dt2 = data[76527-50:76527+51]
-
-for i in range(len(data)):
-    if(data[i]=='a'):
-        data[i]=='A'
-    if(data[i]=='t'):
-        data[i]=='T'
-    if(data[i]=='c'):
-        data[i]=='C'
-    if(data[i]=='g'):
-        data[i]=='G'
-        
-print(dt2)
+for seq in SeqIO.parse("./sample_seq/IRGSP-1.0_genome.fasta", "fasta"):
+	print(seq.id)
+	print(len(seq))
+	print(repr(seq))
+#dt2 = data[76527-50:76527+51]
