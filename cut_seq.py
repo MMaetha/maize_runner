70791e8213355a42b09a0752516598021ee540a3
MMaetha <jiramaetha.s@ku.th>
2018-03-25 21:29:08 +0700

    select chromosome to focus on it

--- cut_seq.py
+++ cut_seq.py
@@ -1,5 +1,9 @@
 from Bio import SeqIO
 from Bio.Seq import Seq
+count=0
 for import_seq in SeqIO.parse("./sample_seq/IRGSP-1.0_genome.fasta", "fasta"):
-	print(repr(import_seq))
+	count = count+1
+	if(count==1):
+		print(import_seq.id)
+		print(repr(import_seq.seq))
 #dt2 = data[76527-50:76527+51]
