e758b9d03c0c2d7e41ecfb634b49529d568c5480
MMaetha <jiramaetha.s@ku.th>
2018-03-25 21:43:45 +0700

    variable value

--- cut_seq.py
+++ cut_seq.py
@@ -1,8 +1,10 @@
 from Bio import SeqIO
 from Bio.Seq import Seq
 count=0
+gap=100
+place=76527
 for import_seq in SeqIO.parse("./sample_seq/IRGSP-1.0_genome.fasta", "fasta"):
 	count = count+1
 	if(count==1):
-		print(import_seq.seq[76527-100:76527+101])
+		print(import_seq.seq[place-gap:place+gap+1])
 #dt2 = data[76527-50:76527+51]
