e268b58a53f79b3e41975e954fbad62c5e784197
MMaetha <jiramaetha.s@ku.th>
2018-03-25 21:37:16 +0700

    cut sequence in fix length

--- cut_seq.py
+++ cut_seq.py
@@ -4,6 +4,5 @@ count=0
 for import_seq in SeqIO.parse("./sample_seq/IRGSP-1.0_genome.fasta", "fasta"):
 	count = count+1
 	if(count==1):
-		print(import_seq.id)
-		print(repr(import_seq.seq))
+		print(import_seq.seq[76527-100:76527+101])
 #dt2 = data[76527-50:76527+51]
