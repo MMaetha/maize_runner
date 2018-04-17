5ab9cb292050072b28f10f9397c6f45b5b3cbd18
MMaetha <jiramaetha.s@ku.th>
2018-04-17 13:14:55 +0700

    find matchScore (blastScore)

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include <vector>
 #include <algorithm>
 #include <cmath>
+#include <string>
 
 using namespace std;
 
@@ -42,28 +43,20 @@ int min(int x, int y, int z){
    	return min(min(x, y), z);
 }
 
-int minEditDist(string str1, string str2){	
+int editDist(string str1 , string str2 , int m ,int n)
+{
+    if (m == 0) return n;
 
-		int m = str1.length();
-		int n = str2.length();
-	    int dp[m+1][n+1];
-	    for (int i=0; i<=m; i++)
-	    {
-	        for (int j=0; j<=n; j++)
-	        {
-	            if (i==0)
-	            	dp[i][j] = j; 
-	            else if (j==0)
-	            	dp[i][j] = i; 
-	            else if (str1[i-1] == str2[j-1])
-	                dp[i][j] = dp[i-1][j-1];
-	            else
-	                dp[i][j] = 1 + min(dp[i][j-1], dp[i-1][j], dp[i-1][j-1]); //Smith-waterman
-	        }
-	    }
-	    int errScore = dp[m][n];
-	    return errScore;
-	}
+    if (n == 0) return m;
+
+    if (str1[m-1] == str2[n-1])
+        return editDist(str1, str2, m-1, n-1);
+    
+    return 1 + min ( editDist(str1,  str2, m, n-1),    // Insert
+                     editDist(str1,  str2, m-1, n),   // Remove
+                     editDist(str1,  str2, m-1, n-1) // Replace
+                   );
+}
 
 void search_map(char* seq)
 {
@@ -228,8 +221,10 @@ int main(int argc, char **argv) {
     }
     
     CloseFASTA(ffp);
-
-  	//printf("Max score is %.2f" (TE.length()-minEditDist(chr[peak],TE))/TE.length());
+    printf("%d",peak);
+    string chr_short = chr.substr(place-200,place+200);
+    double matchScore = editDist(chr_short,TE,chr_short.length(),TE.length());
+  	printf("Max score is %.2f",matchScore);
     fclose(outputFile);
 
     printf("Completed.\n");
